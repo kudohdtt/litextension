6aa4d5b45af5a65fb25d683021a0ea338afb610f
tuantu99 <43340037+kudohdtt@users.noreply.github.com>
2021-07-05 01:27:55 +0700

    fix rewrite url

--- postManager/controller/note.php
+++ postManager/controller/note.php
@@ -1,7 +1,12 @@
 <?php
 class Note{
 	public function __construct($note){
-			echo("Note : ".$note);
+		$strs = NULL;
+		$note = explode("+", $note);
+		foreach ($note as $str) {
+			$strs = $strs." ".$str;
+		}
+			echo("Note : ".$strs);
 			require("../view/admin/note.php");
 		}
 	}
